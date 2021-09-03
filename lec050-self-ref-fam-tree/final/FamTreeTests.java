40d3df8e5c1fa416fc3818b8c24db7a9cad0f46e
Nadeem Abdul Hamid <nadeem@acm.org>
2021-09-03 09:35:27 -0400

    Update FamTreeTests.java
    Added classwork idea

--- lec050-self-ref-fam-tree/final/FamTreeTests.java
+++ lec050-self-ref-fam-tree/final/FamTreeTests.java
@@ -3,17 +3,31 @@ import org.junit.*;
 
 public class FamTreeTests {
 
-    IFamTree bart = new Person("Bart", 
+    IFamTree bart = new Person("Bart",
             new Person("Marge", new Unknown(), new Unknown()),
-            new Person("Homer", 
-                new Unknown(), 
+            new Person("Homer",
+                new Unknown(),
                 new Person("Abe", new Unknown(), new Unknown())));
 
-    IFamTree bart2 = new Person("Bart", 
-            new Person("Marge"), new Person("Homer", 
-                                        new Unknown(), 
+    IFamTree bart2 = new Person("Bart",
+            new Person("Marge"), new Person("Homer",
+                                        new Unknown(),
                                         new Person("Abe")));
-    
+
+    // for class; draw an object diagram:
+    IFamTree unk = new Unknown();
+    IFamTree mlkjr = new Person(
+                "Martin",
+                new Person("Alberta",
+                           new Person("Jenny"),
+                           new Person("Adam", unk, unk)),
+                new Person("Martin",
+                           new Person("Delia",
+                                      unk,
+                                      new Person("Jim")),
+                           new Person("James", unk, unk)));
+
+
     @Test
     public void testCountName() {
         assertEquals(1 , new Person("Bart").countName("Bart"));
